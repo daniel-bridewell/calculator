050e4f516f9b7e62d0b046c5106f9aeb615f6e8e
Danny Bridewell <dcbridewell@gmail.com>
2019-05-20 10:59:01 +1200

    re-push

--- calculator.js
+++ calculator.js
@@ -1,3 +1,18 @@
+// Awful JS Pseudocode
+
+// Create an empty array for future entries 
+
+// Define a 'total' variable and assign the value 0. 
+
+// Define a 'temp' variable and assign an empty string to it. 
+//     - eval(string) can take in a string, like: "2+3*4", and return the resul.
+//     - it's built into JS and will perform the correct order of operations       (BEDMAS) for you.
+
+// Define an 'onclick' event that will execute your following function when a button is pressed. 
+//     - ie. '$("button").on('click', function() {...' 
+
+// Write an if statement that will assign the value of a button pressed to 'val' IF it's a number OR a decimal point. 
+
 const calculate = (n1, operator, n2) => {
   let result = ''
   if (operator === 'add') {
